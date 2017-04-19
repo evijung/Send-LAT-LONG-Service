8afc6bd5b75d4a75996fdb0056e650e9690c584e
evijung <evides01@gmail.com>
2017-04-19 13:20:38 +0700

    Loop for send every 5 minutes

---
+++ app/src/main/java/com/th/samsen/tunyaporn/servicegpscheck/MainActivity.java
@@ -0,0 +1,23 @@
+package com.th.samsen.tunyaporn.servicegpscheck;
+
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Intent serviceIntent = new Intent(MainActivity.this, AutoStartup.class);
+        this.startService(serviceIntent);
+    }
+}
+
---
+++ app/src/main/java/com/th/samsen/tunyaporn/servicegpscheck/MyBroadcastReceiver.java
@@ -0,0 +1,16 @@
+package com.th.samsen.tunyaporn.servicegpscheck;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class MyBroadcastReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getAction().equalsIgnoreCase(Intent.ACTION_BOOT_COMPLETED)) {
+            Intent serviceIntent = new Intent(context, AutoStartup.class);
+            context.startService(serviceIntent);
+        }
+    }
+}
